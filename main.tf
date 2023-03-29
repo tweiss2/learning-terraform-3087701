1ca8233c98420f056c235b5a5f5127e9c144e8a1
tweiss2 <tzippy.weiss@achieve3000.com>
2023-03-29 13:16:50 -0400

    fix error

--- main.tf
+++ main.tf
@@ -84,12 +84,13 @@ module "alb" {
   }
 }
 
+
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "4.17.1"
-  name    = "blog_new"
+  name    = "blog"
 
-  vpc_id  = module.blog_vpc.public_subnets[0]
+  vpc_id  = module.blog_vpc.vpc_id
 
   ingress_rules       = ["http-80-tcp", "https-443-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
