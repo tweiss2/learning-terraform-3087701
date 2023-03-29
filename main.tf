da3db2b54cb4423dabbf99b3c3c446c97c1a0d50
tweiss2 <tzippy.weiss@achieve3000.com>
2023-03-29 13:54:39 -0400

    change formating

--- main.tf
+++ main.tf
@@ -36,15 +36,14 @@ module "autoscaling" {
   version = "6.9.0"
 
   name      = "blog"
-  min_size  = 1
-  max_size  = 2
 
+  min_size            = 1
+  max_size            = 2
   vpc_zone_identifier = module.blog_vpc.public_subnets
   target_group_arns   = module.blog_alb.target_group_arns // what load balancer sends traffic too
   security_groups     = [module.blog_sg.security_group_id]
-  
-  image_id      = data.aws_ami.app_ami.id
-  instance_type = var.instance_type
+  instance_type       = var.instance_type
+  image_id            = data.aws_ami.app_ami.id
 }
 
 
